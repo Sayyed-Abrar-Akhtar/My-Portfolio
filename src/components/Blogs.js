658b3f82cd83eafccb07393ea00959c6c4b26cbc
Sayyed-Abrar-Akhtar <akhtars10@uni.coventry.ac.uk>
2021-05-16 15:23:24 +0545

    Displaying blogs in blog page

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -6,7 +6,16 @@ export const Blogs = ({ blogs, title, showLink }) => {
   return (
     <section className="section">
       <Title title={title} />
-      {blogs.map(blog => console.log(blog))}
+      <div className="section-center blogs-center">
+        {blogs.map(blog => {
+          return <Blog key={blog.id} {...blog} />
+        })}
+      </div>
+      {showLink && (
+        <Link to="/blog" className="btn center-btn">
+          blogs
+        </Link>
+      )}
     </section>
   )
 }
