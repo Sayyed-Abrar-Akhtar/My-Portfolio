68acd238d5630c1c4514aba4ed58dc5042f89a19
Sayyed-Abrar-Akhtar <akhtars10@uni.coventry.ac.uk>
2021-05-18 12:30:44 +0545

    Added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,27 @@
 import React from "react"
+import { FaHeart } from "react-icons/fa"
 import SocialLinks from "../constants/socialLinks"
 const Footer = () => {
-  return <h2>footer component</h2>
+  return (
+    <footer className="footer">
+      <div>
+        <div className="social-link--container">
+          <SocialLinks styleClass="footer-links" />
+        </div>
+        <h4 className="footer-text">
+          Built with{" "}
+          <span>
+            <FaHeart className="footer-heart" />
+          </span>
+        </h4>
+        <h4 className="footer-text">
+          Copyright &copy; {new Date().getFullYear()}{" "}
+          <span className="footer-author">Sayyed Abrar Akhtar</span>
+        </h4>
+        <h4 className="footer-text">All rights reserved</h4>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
