cf88edb6aa70692f248bea57bccba7cb1e8b7880
Sayyed-Abrar-Akhtar <akhtars10@uni.coventry.ac.uk>
2021-05-19 18:22:41 +0545

    removed link

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,7 +2,6 @@ import React from "react"
 import PropTypes from "prop-types"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { FaGithub, FaLink } from "react-icons/fa"
-import { Link } from "gatsby"
 const Project = ({
   id,
   title,
@@ -14,35 +13,7 @@ const Project = ({
   index,
 }) => {
   const pathToImage = getImage(image)
-  return url ? (
-    <Link to={url}>
-      <article className="project">
-        <GatsbyImage image={pathToImage} className="project-img" alt={title} />
-        <div className="project-info">
-          <span className="project-number" key={id}>
-            {index < 10 ? `0${index + 1}` : `${index + 1}`}
-          </span>
-          <h3>{title}</h3>
-          <p className="project-desc">{description}</p>
-          <div className="project-stack">
-            {stack.map((stack, index) => {
-              return <span key={index}>{stack.stack_title}</span>
-            })}
-          </div>
-          <div className="project-links">
-            <a href={github}>
-              <FaGithub className="project-icon" />
-            </a>
-            {url && (
-              <a href={url}>
-                <FaLink className="project-icon" />
-              </a>
-            )}
-          </div>
-        </div>
-      </article>
-    </Link>
-  ) : (
+  return (
     <article className="project">
       <GatsbyImage image={pathToImage} className="project-img" alt={title} />
       <div className="project-info">
